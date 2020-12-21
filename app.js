d506d281f8ad8b9db1ca2d0ecd37c41b8b7cf784
CodingJay90 <akoladefaruq42@gmail.com>
2020-12-21 22:27:57 +0100

    make it better 3

--- app.js
+++ app.js
@@ -53,6 +53,4 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 
-const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log("server running on port " + port));
